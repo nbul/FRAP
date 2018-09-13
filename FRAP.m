f05e845b8bb1fd7a1b4baf31c2866e0191d187bd
nbul <nbul001@gmail.com>
2018-09-13 14:38:13 +0100

    Comments
    the script is now commented

--- FRAP.m
+++ FRAP.m
@@ -20,7 +20,7 @@ dir2 = [filedir, '/Raw data'];
 mkdir(filedir,'/Regions');
 dir3 = [filedir, '/Regions'];
 
-
+% Number of prebleached planes
 Pre_bleach_N1 = inputdlg({'Number of pre-bleach slides:', 'Number of frames'},'Parameters',1,{num2str(0), num2str(0)});
 % Redefine extension
 Pre_bleach_N = str2double(Pre_bleach_N1{1});
@@ -32,6 +32,7 @@ files = dir('*.oib');
 FRAP_final = zeros(N_frames-Pre_bleach_N,numel(files));
 time = 1:390;
 for g=1:numel(files)
+    %% Defining the position of bleached ROI
     ROI_bleach = [1, 2, 3, 4];
     parameters = inputdlg({'Bleach area x_min:', 'y_min:', 'width:', 'height:'},'Parameters',1,{num2str(0), num2str(0),num2str(0),num2str(0)});
     % Redefine extension
@@ -40,6 +41,7 @@ for g=1:numel(files)
     ROI_bleach(3) = str2double(parameters{3});
     ROI_bleach(4) = str2double(parameters{4});
     
+    %% Defining the position of control ROI
     ROI_control = [1, 2, 3, 4];
     parameters = inputdlg({'Control area x_min:', 'y_min:', 'width:', 'height:'},'Parameters',1,{num2str(0), num2str(0),num2str(0),num2str(0)});
     % Redefine extension
@@ -48,6 +50,7 @@ for g=1:numel(files)
     ROI_control(3) = str2double(parameters{3});
     ROI_control(4) = str2double(parameters{4});
     
+    %% Defining the position of background ROI
     ROI_BG = [1, 2, 3, 4];
     parameters = inputdlg({'Enter x_min:', 'y_min:', 'width:', 'height:'},'Parameters',1,{num2str(0), num2str(0),num2str(0),num2str(0)});
     % Redefine extension
@@ -55,7 +58,7 @@ for g=1:numel(files)
     ROI_BG(2) = str2double(parameters{2});
     ROI_BG(3) = str2double(parameters{3});
     ROI_BG(4) = str2double(parameters{4});
-    
+    %% Saving ROIs
     cd(dir3);
     Otput_regions = ['embryo ', num2str(g), '_regions.csv'];
     data_regions = [ROI_bleach', ROI_control', ROI_BG'];
@@ -63,6 +66,7 @@ for g=1:numel(files)
     csvwrite_with_headers(Otput_regions,data_regions, headers);
     cd(filedir);
     
+    %% Reading FRAP movie in .oib format
     Image_name = [num2str(g),'.oib'];
     Image = bfopen(Image_name);
     
@@ -73,7 +77,7 @@ for g=1:numel(files)
     FRAP1 = zeros(seriesCount,1);
     Control = zeros(seriesCount,1);
     BG = zeros(seriesCount,1);
-    
+    %% Collecting information about mean intensity of each ROI at each time point
     for q=1:seriesCount
         Image_time = Series{q,1};
         Image_FRAP = imcrop(Image_time, ROI_bleach);
@@ -87,20 +91,24 @@ for g=1:numel(files)
     
     %imshow(FRAP_1D,[min(min(FRAP_1D)) max(max(FRAP_1D))])
     
+    %% Normalisation
+    % backround subtraction
     FRAP_corr = FRAP1- BG;
     Control_corr = Control - BG;
     
+    % normalization to control area intensity at the same time point
     FRAP_norm = FRAP_corr./Control_corr;
-    
+    % average pre-bleach
     Pre_bleach = mean(FRAP_norm(1:10));
-    
+    % normalization to average prebleach intensity
     FRAP_norm = FRAP_norm/Pre_bleach;
-    
+    % Removing pre-bleach datapoints
     FRAP_norm = FRAP_norm(Pre_bleach_N+1:seriesCount);
-    
+    % normalization to bleach amplitude
     FRAP_final(:,g) = (FRAP_norm-FRAP_norm(1))/(Pre_bleach-FRAP_norm(1));
    
-    
+    %% Plotting results and saving the data
+    % Image with the recovery curve for each movie
     image3 = figure;
     set(axes,'FontSize',16);
     plot(time,FRAP_final(:,g), 'o', 'Color','b', 'MarkerSize',4, 'MarkerFaceColor', 'b');
@@ -114,7 +122,8 @@ for g=1:numel(files)
     cd(dir1);
     Otput_Image = ['embryo ', num2str(g), '.tif'];
     print(image3, '-dtiff', '-r300', Otput_Image);
-    
+    % Mean intensities of each ROI at each time point for future
+    % reprocessing
     cd(dir2);
     Otput_data = ['embryo ', num2str(g), '.csv'];
     data = [FRAP1, Control, BG];
@@ -123,7 +132,7 @@ for g=1:numel(files)
     cd(filedir);
     close all;
 end
-
+%% Saving normalized data for all movies
 csvwrite('Result.csv', FRAP_final);
 
 cd(currdir);
